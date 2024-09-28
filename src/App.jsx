5f98a2fc646187ad5647746e1ac4f1103f0c516b
ninico2321 <niasn2778@gmail.com>
2024-09-28 04:58:34 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+function App() {
+  
+  function mensagemNias() {
+    const mensagem = encodeURIComponent("Opaaa, resenha hj me pega!"); 
+    const numero = "55986468805";
+    const linkWhatsApp = `https://wa.me/${numero}?text=${mensagem}`;
+    window.open(linkWhatsApp, "_blank");
+  }
+  function respostaErrada(){
+    alert("ah ta trolando po ,sifude")
+  }
+
+  return (
+    <><div className="body">
+      <div className="containerPrincipal">
+        <div className="container">
+          <h1>Resenhazinha </h1><h1>hoje</h1> <h1>te pega nao? </h1></div>
+        </div>
+        <h1 className="sugestao"> Sugestões </h1>
+        <div className="sugestoes">
+        <h2>Drinks</h2>
+        <h2> Jogatina</h2>
+        <h2>Trapaça</h2>
+      </div>
+              <div className="acima"><h1>PUTAS ACIMA DO PESO </h1></div>
+       <div className="botao"> <button className="sim" onClick={mensagemNias}>yes</button>
+        <button className="nao" onClick={respostaErrada}>no</button></div> </div>
+    </>
+  )
+}
+
+export default App
+
+    
+    
